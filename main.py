fd0f02df2f6a0e436031fd8ed299f296715712c3
Jared Dye <jjtd2000@gmail.com>
2024-11-04 18:08:41 -0500

    getting ui started

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-from db import Database
-from user import User
+from nicegui import ui
 
-db = Database("test.db")
-db.generate_tables()
-user = User()
+from ui_elements.ui import UI
 
-while True:
-  option = input("1. Register New Account\n2. Log In\n3. Reset Password\n")
-  if option == "1":
-    print("----REGISTRATION----")
-    username = input("Please enter your username:")
-    password = input("Please enter your password:")
-    user.register(username, password)
-  if option == "2":
-    print("----LOG IN----")
-    username = input("Please enter your username:")
-    password = input("Please enter your password:")
-    verified = user.login(username, password)
-    if verified:
-      option = input("1. Add Vault\n")
-      if option == "1":
-        print("----ADD VAULT----")
-        name = input("Please enter vault name:")
-        user.add_vault(name)
-  if option == "3":
-    print("----RESET PASSWORD----")
-    username = input("Please enter your username:")
-    password = input("Please enter your password:")
-    verified = user.login(username, password)
-    if verified:
-      print("----RESET PASSWORD----")
-      new_pw = input("Please enter new password:")
-      user.change_password(new_pw)
+
+my_ui = UI()
 
 
